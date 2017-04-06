5aa0d53ee20635bc0be1aca270bf2d2fe7194bd2
Su Gi Chandran <chandran.su.gi@gmail.com>
2017-04-06 00:06:47 -0700

    Implementing creating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_login
+  before_action :require_login, only: [:index, :show]
   before_action :require_admin, only: [:index]
 
   def index
@@ -13,4 +13,23 @@ class UsersController < ApplicationController
     @title = "User " + params[:id].to_s
     render :show
   end
+
+  def new
+    render :new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/users/' + user.id.to_s
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :admin, only: :index
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :create]
 
+  get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
