ec7679b0e50b1ff57a7389244f7c077d94c0813c
Su Gi Chandran <chandran.su.gi@gmail.com>
2017-04-06 06:33:13 -0700

    Added logout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
+
+  def logged_in?
+    current_user != nil
+  end
 
   def require_login
     redirect_to '/login' unless current_user
   end
 
-  # Use this with require_login
   def require_admin
-    if current_user.role != 'Admin'
+    if current_user.role.name != 'Admin'
       render :text => 'Not Found', :status => '404'
     end
   end
